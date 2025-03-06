7ee7386e381948ec7b59f965b94814a644fde5d8
Favour Wanjui <wanjui.dev@gmail.com>
2025-03-06 22:49:26 +0300

    frontend API route changed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import Cookies from "js-cookie"
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8085/api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://3.83.154.20:8000/api"
 
 const api = axios.create({
   baseURL: API_URL,
